a05c23651fa1d52790ce9d3fcf9827e714caf263
Lacky227 <tishko.artyr@gmail.com>
2025-03-03 23:57:43 +0200

    feat(controller-UserController): added register method

--- userservice/src/main/java/org/veedev/userservice/controller/UserController.java
+++ userservice/src/main/java/org/veedev/userservice/controller/UserController.java
@@ -1,9 +1,19 @@
 package org.veedev.userservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.veedev.userservice.dto.RegisterRequest;
+import org.veedev.userservice.service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+        return userService.register(registerRequest);
+    }
 }
