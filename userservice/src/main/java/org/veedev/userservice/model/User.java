35efad8bde4216c49a18ae8c52c28b94644c1418
Lacky227 <tishko.artyr@gmail.com>
2025-03-03 22:50:47 +0200

    feat(model-user, pom.xml): added Lombok and create User model

---
+++ userservice/src/main/java/org/veedev/userservice/model/User.java
@@ -0,0 +1,28 @@
+package org.veedev.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
