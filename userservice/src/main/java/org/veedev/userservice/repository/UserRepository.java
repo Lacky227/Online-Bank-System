4a129fcb32fa7b70f027dc3a30d42838a799c732
Lacky227 <tishko.artyr@gmail.com>
2025-03-03 23:24:39 +0200

    feat(repository-UserRepository): added findByUsername and findByEmail methods

--- userservice/src/main/java/org/veedev/userservice/repository/UserRepository.java
+++ userservice/src/main/java/org/veedev/userservice/repository/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 import org.veedev.userservice.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
