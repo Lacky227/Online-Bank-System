679d3921d0b3d3e1e3f8597fd0cab7ca726ae88c
Lacky227 <tishko.artyr@gmail.com>
2025-03-04 00:04:45 +0200

    feat(config-SecurityConfig): configure security filter chain and permit registration endpoint

--- userservice/src/main/java/org/veedev/userservice/config/SecurityConfig.java
+++ userservice/src/main/java/org/veedev/userservice/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package org.veedev.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/user/register").permitAll();
+                    auth.anyRequest().authenticated();
+                }).formLogin(Customizer.withDefaults())
+                .build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
