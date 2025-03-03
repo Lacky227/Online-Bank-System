409e6a3a010bffea5d00bcf4063af977be6fb794
Lacky227 <tishko.artyr@gmail.com>
2025-03-03 23:19:10 +0200

    feat(repository-UserRepository, application.properties): create UserRepository and configure database connection

---
+++ userservice/src/main/java/org/veedev/userservice/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.veedev.userservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.veedev.userservice.model.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
