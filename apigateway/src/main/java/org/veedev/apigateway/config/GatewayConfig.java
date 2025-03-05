92ec89d30bba6abc18a034db7b4e603cee5af7d9
Lacky227 <tishko.artyr@gmail.com>
2025-03-05 21:27:24 +0200

    feat(config-GatewayConfig): add route configuration for user_service

--- apigateway/src/main/java/org/veedev/apigateway/config/GatewayConfig.java
+++ apigateway/src/main/java/org/veedev/apigateway/config/GatewayConfig.java
@@ -1,4 +1,16 @@
 package org.veedev.apigateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GatewayConfig {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user_service", r -> r.path("/user/**").uri("http://localhost:8081/"))
+                .build();
+    }
 }
